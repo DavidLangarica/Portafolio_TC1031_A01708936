293ab8e664dcc63a0a1aa29e99f2b23ca1d22361
David Langarica <105810565+DavidLangarica@users.noreply.github.com>
2022-10-18 21:31:33 -0500

    Added comments to classes.h

--- Act 2.3/classes.h
+++ Act 2.3/classes.h
@@ -2,7 +2,7 @@
 //
 // File: classes.h
 // Autores: David René Langarica Hernández - A01708936, Sebastian Flores Lemus - A01709229
-// Description: Archivo de clases empleadas para el funcionamiento del programa
+// Description: 
 //
 // Copyright (c) 2022 by Tecnologico de Monterrey.
 // All Rights Reserved. May be reproduced for any non-commercial
@@ -24,6 +24,12 @@ using std::ifstream;
 using std::ofstream;
 using std::string;
 
+// Usamos clases para romper el string y usar sus componentes a lo largo de la actividad
+
+/* clase ShipReg tiene tiene los atributos de mes, puerto y ubicacion de cada barco
+ Sus metodos son getters que se usan a lo largo del programa 
+*/
+
 class ShipReg{
 	private:
 		int month;
@@ -34,6 +40,7 @@ class ShipReg{
 		ShipReg(){};
 		ShipReg(int,string,string);
 
+    //getters
 		int getMonth(){
 			return month;
 		};
@@ -47,6 +54,8 @@ class ShipReg{
 		};
 };
 
+// Constructor de clase ShipReg(int, string, string)
+
 ShipReg::ShipReg(int month_, string encryp_, string ubi_)
 {
 	month = month_;
@@ -54,17 +63,28 @@ ShipReg::ShipReg(int month_, string encryp_, string ubi_)
 	ubi = ubi_;
 }
 
+/*
+ clase month tiene como atributos Mctr, Rctr y dos vectores, separamos los datos basado en el puerto de entrada de cada barco, por esa razon usamos dos vectores de registro diferentes.
+En metodos usamos getters ademas de funciones para agregar elementos a los registros, para formato entre otros.
+*/
+
+
 class Month{
 	private:
 		int num;
+    // contadores por puerto
 		int Mctr = 0;
 		int Rctr = 0;
+
+  //vectores de registro
+
 		vector<ShipReg> sregsM;
 		vector<ShipReg> sregsR;
 
 	public:
 		Month(int);
 
+    // funcion para sumar al contador de 'M' o 'R' cuando aplique
 		void sumCtr(int bdef){
 			if (bdef == 0){
 				Mctr += 1;
@@ -74,6 +94,7 @@ class Month{
 			}
 		};
 
+    // getters
 		int getMctr(){
 			return Mctr;
 		};
@@ -82,6 +103,8 @@ class Month{
 			return Rctr;
 		};
 
+    // Agregamos elementos al vector de registro (R o M)
+
 		void addSregM(ShipReg sreg_){
 			sregsM.push_back(sreg_);
 		};
@@ -89,6 +112,8 @@ class Month{
 		void addSregR(ShipReg sreg_){
 			sregsR.push_back(sreg_);
 		};
+
+    // getters
 
 		string getUBIM(int pos){
 			return sregsM[pos].getUBI();
@@ -106,24 +131,30 @@ class Month{
 			return sregsR.size();
 		};
 
+    // formato para imprimir los resultados
+
 		void output(ofstream& out_file){
 
+      // imprime solo si hay mas de una ocurrencia en M
 			if(getMctr() > 0){
 				out_file << "M " << getMctr() << ": ";
 
 				for (int m = 0; m < getSregSizeM(); m++)
 				{
+          // imrprimimos en archivo externo
 					out_file << getUBIM(m) << " ";
 				};
 
 				out_file << '\n';
 			}
 
+      // imprime solo si hay mas de una ocurrencia en R
 			if(getRctr() > 0){
 				out_file << "R " << getRctr() << ": ";
 
 				for (int f = 0; f < getSregSizeR(); f++)
 				{
+          // imrprimimos en archivo externo
 					out_file << getUBIR(f) << " ";
 				};
 
@@ -131,6 +162,7 @@ class Month{
 			}
 		};
 
+    // calcula el número de registros para cada puerto y los almacena en un subvector de 'M' o 'R'
 		void ports(vector<ShipReg> sreg_vec){
 			for (int j = 0; j < sreg_vec.size(); j++){
 				
@@ -150,6 +182,9 @@ class Month{
 			}
 		};
 };
+
+          
+// constructor de clase Month
 
 Month::Month(int num_)
 {
